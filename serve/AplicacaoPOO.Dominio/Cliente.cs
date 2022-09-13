31363266279ca0554533dbee38b3b69dfff4ac04
vanderlei <vanderlei.f.silva18@gmail.com>
2022-09-13 20:54:28 -0300

    Closed: #1

---
+++ serve/AplicacaoPOO.Dominio/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicacaoPOO.Dominio
+{
+    public class Cliente
+    {
+        string Nome { get; set; }
+        string Cpf { get; set; }
+        Debito debito { get; set; }
+        Credito credito { get; set; }
+    }
+}
---
+++ serve/AplicacaoPOO.Dominio/Credito.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicacaoPOO.Dominio
+{
+    public class Credito
+    {
+        Pagamento Pagamento { get; set; }
+        int Parcelas { get; set; }
+        float Juros { get; set; }
+    }
+}
